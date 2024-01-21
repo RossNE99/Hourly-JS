49d13fd410e59d946ea20bf756cf462e01075936
Ross <rossycharlton@googlemail.com>
2024-01-21 01:29:23 +0000

    Added document.ready function to reduce errors#

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,54 @@
-dayjs.extend(window.dayjs_plugin_advancedFormat)
-var currentDay = dayjs().format("dddd, MMMM Do")
+$(document).ready(function() {
+    dayjs.extend(window.dayjs_plugin_advancedFormat)
+    var currentDay = dayjs().format("dddd, MMMM Do")
 
-$("#currentDay").text(currentDay)
+    $("#currentDay").text(currentDay)
 
-function getHoursBetween(start, end) {
-    const startHour = dayjs(start).startOf('hour');
-    const endHour = dayjs(end).startOf('hour');
-  
-    const hoursArray = [];
-    let currentHour = startHour;
-  
-    while (currentHour.isBefore(endHour)) {
-      hoursArray.push(currentHour);
-      currentHour = currentHour.add(1, 'hour');
+    function getHoursBetween(start, end) {
+        const startHour = dayjs(start).startOf('hour');
+        const endHour = dayjs(end).startOf('hour');
+    
+        const hoursArray = [];
+        let currentHour = startHour;
+    
+        while (currentHour.isBefore(endHour)) {
+        hoursArray.push(currentHour);
+        currentHour = currentHour.add(1, 'hour');
+        }
+    
+        return hoursArray;
     }
-  
-    return hoursArray;
-  }
 
-const startTime = '2024-01-20 09:00:00'; //this will be replaced, only for testing
-const endTime = '2024-01-20 17:00:00';  //this will be replaced, only for testing
+    const startTime = '2024-01-20 09:00:00'; //this will be replaced, only for testing
+    const endTime = '2024-01-20 17:00:00';  //this will be replaced, only for testing
 
-function renderPlanner() {
-    $.each(getHoursBetween(startTime, endTime), function( index, value ) {
-        var hourDiv = $("<div>", {
-            class: "hour col-1",
-            text: value.format('hA')
-        })
-        var textDivColor = dayjs().isAfter(value, 'hour') ? "past" : dayjs().isSame(value, 'hour') ? "present" : "future"
-        var discriptionText =  $("<textarea>", {
-            class: `description col ${textDivColor}`,
-            text: ""
-        })
+    function renderPlanner() {
+        $.each(getHoursBetween(startTime, endTime), function( index, value ) {
+            var hourDiv = $("<div>", {
+                class: "hour col-1",
+                text: value.format('hA')
+            })
+            var textDivColor = dayjs().isAfter(value, 'hour') ? "past" : dayjs().isSame(value, 'hour') ? "present" : "future"
+            var discriptionText =  $("<textarea>", {
+                class: `description col ${textDivColor}`,
+                text: ""
+            })
 
-        var saveButtonDiv = $("<div>", {
-            class: "saveBtn col-1"
-        })
-        
-        var timeBloclkRow = $("<div>", {
-            class: "row",
-        })
+            var saveButtonDiv = $("<div>", {
+                class: "saveBtn col-1",
+            })
+            
+            var timeBloclkRow = $("<div>", {
+                class: "row",
+            })
 
-        $(timeBloclkRow).append(hourDiv)
-        $(timeBloclkRow).append(discriptionText)
-        $(timeBloclkRow).append(saveButtonDiv)
+            $(timeBloclkRow).append(hourDiv)
+            $(timeBloclkRow).append(discriptionText)
+            $(timeBloclkRow).append(saveButtonDiv)
 
-        $(".container").append(timeBloclkRow)
-    });
-}
+            $(".container").append(timeBloclkRow)
+        });
+    }
 
-renderPlanner()
+    renderPlanner()
+})
